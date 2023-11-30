b6e2671d6d4d801bd3ad0a13be751cb111770990
zooeypeng <zooey.peng@gmail.com>
2023-11-30 17:35:59 +0800

    improve route UX.

--- src/Left.js
+++ src/Left.js
@@ -1,15 +1,22 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from './AuthContext';
 import './Left.css';
 
 function Left() {
+  const navigate = useNavigate();
   const { logout, isLoggedIn } = useAuth();
+
+  const handleLogout = () => {
+    logout();
+    navigate('/');
+  }
+
   return (
     <div className="Left">
       <Link className="menu" to="/">About Us</Link>
       { isLoggedIn && <Link className="menu" to="/info">System Info</Link> }
       { isLoggedIn
-        ? <div className="menu auth" onClick={logout}>Logout</div>
+        ? <div className="menu auth" onClick={handleLogout}>Logout</div>
         : <Link className="menu auth" to="/login">Login</Link>
       }
     </div>
